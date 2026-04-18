9f26fe1b7e24bf109cf842a94621347c93d6fb29
DiegoaARM <juandiegoduquejimenez@gmail.com>
2026-04-17 19:07:28 -0500

    Reorganizando el nombre del modulo y nombre de carpetas. Ahora este es el trackinvest-account-service

--- src/main/java/com/trackinvest/account/TrackInvestAccountServiceApplication.java
+++ src/main/java/com/trackinvest/account/TrackInvestAccountServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TrackinvestApplication {
+public class TrackInvestAccountServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TrackinvestApplication.class, args);
+		SpringApplication.run(TrackInvestAccountServiceApplication.class, args);
 	}
 
 }
